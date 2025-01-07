d89182f574810edac684f717700dacfb6dff7a3a
LeoLei8060 <18157100608@163.com>
2025-01-07 19:47:23 +0800

    优化马赛克绘制效果

--- src/shapes/mosaic.h
+++ src/shapes/mosaic.h
@@ -2,37 +2,38 @@
 #define MOSAIC_H
 
 #include "shape.h"
-#include <QVector>
-#include <QPoint>
 #include <QRect>
-#include <QColor>
+#include <QImage>
+#include <QVector>
 
 class Mosaic : public Shape
 {
 public:
     enum class MosaicType {
-        Circle,
-        Rectangle
+        Square = 0,     // 方形马赛克
+        Circle = 1      // 圆形马赛克
     };
 
-    Mosaic(const QPoint& startPoint, MosaicType type = MosaicType::Rectangle, const QColor& color = Qt::black);
-    ~Mosaic() override = default;
-
+    Mosaic(const QPoint& startPoint, MosaicType type = MosaicType::Square, const QColor& color = Qt::black);
     void draw(QPainter& painter) const override;
     void updateShape(const QPoint& pos) override;
     bool isComplete() const override;
     Shape* clone() const override;
 
-private:
-    QVector<QPoint> points;
-    MosaicType type;
-    QColor color;
-    bool complete = false;
-    static constexpr int MOSAIC_SIZE = 10;  // 马赛克大小
-    static constexpr int MIN_DISTANCE = 5;  // 两个马赛克之间的最小距离
+    void setBackground(const QPixmap& bg) { m_background = bg; }
+    void updateMosaicArea(const QPoint& pos);
 
-    void drawMosaicUnit(QPainter& painter, const QPoint& center) const;
-    bool isTooClose(const QPoint& newPoint) const;
+private:
+    QVector<QRect> m_mosaicAreas;     // 存储所有马赛克区域
+    QVector<QImage> m_mosaicImages;    // 存储所有马赛克图像
+    QPoint m_lastPoint;                // 上一个点的位置
+    bool m_complete = false;
+    MosaicType m_mosaicType;
+    QPixmap m_background;             // 背景图像
+
+    static constexpr int BLOCK_SIZE = 10;   // 马赛克方块大小
+    static constexpr int BRUSH_SIZE = 20;   // 马赛克笔刷大小
+    QColor getAverageColor(const QImage& image, const QRect& rect) const;
 };
 
 #endif // MOSAIC_H
